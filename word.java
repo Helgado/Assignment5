71070dbf81f7ba5116d41f299e4751869ad679e7
Helgado <Harley.Delgado001@umb.edu>
2015-11-23 20:55:20 -0500

    Create word.java

---
+++ word.java
@@ -0,0 +1,8 @@
+package Wordcount;
+public class Word{
+    public int frequency;
+    //the amount of times the word comes up in the file
+    public String wordcap;
+    public String wordnocap;
+    //the word with and without capitilization
+}
