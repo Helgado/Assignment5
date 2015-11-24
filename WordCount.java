c5ad8e86bce4b94b55e8bb5c59d46dc7974b7eec
Helgado <Harley.Delgado001@umb.edu>
2015-11-23 20:56:54 -0500

    Create WordCount.java
    Main class

---
+++ WordCount.java
@@ -0,0 +1,18 @@
+package Wordcount;
+public class WordCount{
+    public static void main(String[] args){
+        getfile();
+        addwords();
+        sortandprint();
+    }
+    
+    public static void getfile(){
+    }
+    
+    public static void addwords(){
+    }
+    
+    public static void sortandprint(){
+    }
+    
+}
