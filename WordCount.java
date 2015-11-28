99bd952f7dda3bcd2ac256406765f6f7e7beeddc
Helgado <Harley.Delgado001@umb.edu>
2015-11-28 13:33:11 -0500

    Update WordCount.java

--- WordCount.java
+++ WordCount.java
@@ -6,35 +6,63 @@ import java.util.Scanner;
 public class WordCount{
     public static void main(String[] args){
         String filename = getfile();
-        readfile(filename);
-        addwords();
-        sortandprint();
+        int c = countfile(filename);
+        System.out.println(c);
+        Word[] unfiltered = readfile(filename, c);
+        filterwords(unfiltered);
+        printwords();
     }
     
     public static String getfile(){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Filename: ");
         String filename = input.nextLine();
+        input.close();
         return filename;
     }
     
-    public static void readfile(String filename){
-        try{
-            File file = new File(filename);
-            Scanner input = new Scanner(file);
-            while (input.hasNext()) {
-                System.out.printf("%s ", input.next());
+    @SuppressWarnings("finally")
+	public static int countfile(String filename){
+    	int counter = 0;
+    	try{
+    	File file = new File(filename);
+    	Scanner input = new Scanner(file);
+    	while (input.hasNext()) {
+    		String lel = input.next();
+    		counter++;
+    	}
+    	} finally{
+    	return counter;
+    	}
+    }
+    
+    public static Word[] readfile(String filename, int c){
+    	Word[] array = new Word[c];
+    	try{
+        	File file = new File(filename);
+        	Scanner input = new Scanner(file);
+        	while (input.hasNext()) {
+        		for (int i = 0; i < c; i++){
+        			array[i].wordstring = input.next();
+        		}
+        	}
+        	} finally{
+        	return array;
+        	}
+        }
+     
+    
+    public static void filterwords(Word[] array){
+    	for (int i = 0; i < array.length; i++){
+            for (int j = i; j > 0; j--){
+                if (array[i] == array[j-1]){
+                    array[j-1].frequency++
+                    break;
             }
         }
-        catch (FileNotFoundException e){
-            System.out.println(e.getMessage());
-        }
     }
     
-    public static void addwords(){
+    public static void printwords(){
     }
-    
-    public static void sortandprint(){
-    }
-    
+  
 }
