3df5d4591b814e1c76a2cf2a0afd55332286e0eb
Helgado <Harley.Delgado001@umb.edu>
2015-11-28 13:32:10 -0500

    Update word.java

--- word.java
+++ word.java
@@ -1,8 +1,9 @@
 package Wordcount;
 public class Word{
     public int frequency;
-    //the amount of times the word comes up in the file
-    public String wordcap;
-    public String wordnocap;
-    //the word with and without capitilization
-}
+    public String wordstring;
+    public Word(int frequency, String wordstring){
+    	this.frequency = frequency;
+    	this.wordstring = wordstring;
+    }
+    }
