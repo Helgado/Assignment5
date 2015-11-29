c625779b8cdec99de872d32637aa3ecedcbe2f45
Helgado <Harley.Delgado001@umb.edu>
2015-11-29 15:53:19 -0500

    Update word.java

--- word.java
+++ word.java
@@ -1,9 +1,12 @@
-package Wordcount;
+package pack;
+
 public class Word{
     public int frequency;
+    //the amount of times the word comes up in the file
     public String wordstring;
+    //the word itself
     public Word(int frequency, String wordstring){
     	this.frequency = frequency;
     	this.wordstring = wordstring;
     }
-    }
+}
